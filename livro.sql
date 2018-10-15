7b0ab922d8191feea147d48af8e700a22b975444
joseferreira01 <joseifpb2015@gmail.com>
2018-10-15 00:04:23 -0300

    inset livro ok

---
+++ livro.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO public.livro(
+            id, descricao, editora, titulo)
+    VALUES ('Acvve', 'beerrb', 'moderna', 'java 7');
+    INSERT INTO public.livro(
+            id, descricao, editora, titulo)
+    VALUES ('Acvve4', 'ffff', 'moderna', 'java 9');
+    INSERT INTO public.livro(
+            id, descricao, editora, titulo)
+    VALUES ('Acvve5', 'dddd', 'moderna', 'java 10');
+
+
